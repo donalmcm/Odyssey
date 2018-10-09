30af4780d76a3cdef8698c4f81add25101175c60
Dónal McManus <donal.mcm@gmail.com>
2018-10-09 19:26:59 +0100

    creation of file structure (with use of empty files)

---
+++ ProjectOdyssey/src/main/objects/Employee.java
@@ -0,0 +1,14 @@
+package objects
+
+public class Empyloee (
+
+        class Employee {
+            private String name;
+            private String title;
+            private String email;
+            private List<String> skills = new ArrayList<>();
+            //availabilty
+
+        }
+
+        )
