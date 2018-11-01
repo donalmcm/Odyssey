b430b5096f51f7327861eb914fac975be629814a
Dónal McManus <donal.mcm@gmail.com>
2018-11-01 22:25:46 +0000

    node_modules folder added, further set-up in progress for spring implementation

---
+++ projectOdyssey/src./main/java/zcon/odyssey/projectOdyssey/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package zcon.odyssey.projectOdyssey;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
---
+++ projectOdyssey/src./main/java/zcon/odyssey/projectOdyssey/ProjectOdysseyApplication.java
@@ -0,0 +1,12 @@
+package zcon.odyssey.projectOdyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOdysseyApplication.class, args);
+	}
+}
