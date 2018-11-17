fbe658f251ef2817a488c5e2de2d1d7e18e2f04b
Dónal McManus <donal.mcm@gmail.com>
2018-11-17 17:21:12 +0000

    removal of @Transactional from repositories

--- projectOdyssey/src./main/java/zcon/odyssey/projectOdyssey/EmployeeRepository.java
+++ projectOdyssey/src./main/java/zcon/odyssey/projectOdyssey/EmployeeRepository.java
@@ -1,11 +1,6 @@
 package zcon.odyssey.projectOdyssey;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import javax.transaction.Transactional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- projectOdyssey/src/main/java/zcon/odyssey/projectOdyssey/OdysseyMeetingRepository.java
+++ projectOdyssey/src/main/java/zcon/odyssey/projectOdyssey/OdysseyMeetingRepository.java
@@ -2,6 +2,5 @@ package zcon.odyssey.projectOdyssey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface OdysseyMeetingRepository extends JpaRepository<OdysseyMeeting, Long> {
 }
