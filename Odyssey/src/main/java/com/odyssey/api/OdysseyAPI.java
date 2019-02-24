7f6cdee6c0e17de535e21b62773c1f8c56140104
Dónal McManus <donal.mcm@gmail.com>
2019-02-24 17:19:25 +0000

    named queries added and get api's added for odysseys

--- Odyssey/src/main/java/com/odyssey/api/OdysseyAPI.java
+++ Odyssey/src/main/java/com/odyssey/api/OdysseyAPI.java
@@ -6,13 +6,14 @@ import com.HibernateUtils;
 import com.odyssey.model.Topic;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/odyssey")
+@Path("/odysseys")
 public class OdysseyAPI {
 
     SessionFactory factory = HibernateUtils.getSessionFactory();
@@ -20,16 +21,97 @@ public class OdysseyAPI {
 
     @GET
     @Produces("application/json")
-    public String getAllOdysseys() {
-        return " ";
+    public Response getAllOdysseys() {
+
+        //try{
+        session.getTransaction().begin();
+
+        Query<Odyssey> query = session.createNamedQuery("Odyssey.findAllOdysseys",Odyssey.class);
+        List<Odyssey> odysseys = query.getResultList();
+
+        session.getTransaction().commit();
+        session.close();
+        return Response.ok(odysseys, MediaType.APPLICATION_JSON).build();
+
+//        } catch (Exception e) {
+//
+//            return e.printStackTrace();
+//            session.getTransaction().rollback();
+//        }
     }
 
-    // create an employee
+    @GET
+    @Path("/{topic}")
+    @Produces("application/json")
+    public Response getAllOdysseysByTopic(@PathParam("topic")String topic) {
+
+        //try{
+        session.getTransaction().begin();
+
+        Query<Odyssey> query = session.createNamedQuery("Odyssey.findOdysseysByTopic",Odyssey.class);
+        List<Odyssey> odysseys = query.getResultList();
+
+        session.getTransaction().commit();
+        session.close();
+        return Response.ok(odysseys, MediaType.APPLICATION_JSON).build();
+
+//        } catch (Exception e) {
+//
+//            return e.printStackTrace();
+//            session.getTransaction().rollback();
+//        }
+    }
+
+    // will be used to display the current users mentor odysseys
+    @GET
+    @Path("/{mentor}")
+    @Produces("application/json")
+    public Response getAllOdysseysByMentor(@PathParam("mentor")String mentor) {
+
+        //try{
+        session.getTransaction().begin();
+
+        Query<Odyssey> query = session.createNamedQuery("Odyssey.findOdysseysByMentor",Odyssey.class);
+        List<Odyssey> odysseys = query.getResultList();
+
+        session.getTransaction().commit();
+        session.close();
+        return Response.ok(odysseys, MediaType.APPLICATION_JSON).build();
+
+//        } catch (Exception e) {
+//
+//            return e.printStackTrace();
+//            session.getTransaction().rollback();
+//        }
+    }
+    @GET
+    @Path("/{mentee}")
+    @Produces("application/json")
+    public Response getAllOdysseysByMentee(@PathParam("mentee")String mentee) {
+
+        //try{
+        session.getTransaction().begin();
+
+        Query<Odyssey> query = session.createNamedQuery("Odyssey.findOdysseysByMentee",Odyssey.class);
+        List<Odyssey> odysseys = query.getResultList();
+
+        session.getTransaction().commit();
+        session.close();
+        return Response.ok(odysseys, MediaType.APPLICATION_JSON).build();
+
+//        } catch (Exception e) {
+//
+//            return e.printStackTrace();
+//            session.getTransaction().rollback();
+//        }
+    }
+
+    // create an odyssey
     @POST
     @Path("create")
     @Consumes("application/json")
     @Produces("application/json")
-    public Response createEmployee(Employee mentor,Employee mentee, Topic topic) {
+    public Response createOdyssey(Employee mentor,Employee mentee, Topic topic) {
         session.getTransaction().begin();
 
         Odyssey newOdyssey = new Odyssey(mentor,mentee,topic);
--- Odyssey/src/main/java/com/odyssey/model/Odyssey.java
+++ Odyssey/src/main/java/com/odyssey/model/Odyssey.java
@@ -8,7 +8,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
 @NamedQueries({ @NamedQuery(name = "Odyssey.findAllOdysseys", query = "select o from Odyssey o"),
-        @NamedQuery(name = "Odyssey.findByOdysseyId", query = "select o from Odyssey o where o.id=:id"), })
+        @NamedQuery(name = "Odyssey.findByOdysseyId", query = "select o from Odyssey o where o.id=:id"),
+        @NamedQuery(name = "Odyssey.findOdysseysByMentor", query = "select o from Odyssey o where o.mentor=:mentor"),
+        @NamedQuery(name = "Odyssey.findOdysseysByMentee", query = "select o from Odyssey o where o.mentee=:mentee"),
+        @NamedQuery(name = "Odyssey.findOdysseysByTopic", query = "select o from Odyssey o where o.topic=:topic")})
 
 @XmlRootElement
 @Entity
@@ -70,10 +73,40 @@ public class Odyssey {
         return percentageComplete;
     }
 
+    // Generated Getters and Setters
     public long getId() {
         return id;
     }
-
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Employee getMentor() {
+        return mentor;
+    }
+    public void setMentor(Employee mentor) {
+        this.mentor = mentor;
+    }
+    public Employee getMentee() {
+        return mentee;
+    }
+    public void setMentee(Employee mentee) {
+        this.mentee = mentee;
+    }
+    public boolean isActive() {
+        return isActive;
+    }
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+    public List<OdysseyMeeting> getOdysseyMeetings() {
+        return odysseyMeetings;
+    }
+    public void setOdysseyMeetings(List<OdysseyMeeting> odysseyMeetings) {
+        this.odysseyMeetings = odysseyMeetings;
+    }
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
     public Topic getTopic() {
         return topic;
     }
